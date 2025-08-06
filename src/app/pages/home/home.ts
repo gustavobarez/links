75de65d87a3f772bdf608f3162816cbda923afdc
gustavobarez <gbarez44@gmail.com>
2025-08-06 11:11:16 -0300

    feat: add typing to role text,  larger container to links and some QoL

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,11 +21,16 @@ import { Theme } from '../../services/theme';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit {
+export class Home implements OnInit, OnDestroy {
   textData: any = {
     pt: {
       name: 'Gustavo Barez',
-      title: 'Engenheiro de Software',
+      titles: [
+        'Engenheiro de Software',
+        'Desenvolvedor Full Stack',
+        'Desenvolvedor Frontend',
+        'Desenvolvedor Backend',
+      ],
       bio: 'Brasileiro obcecado por tecnologia há uma década. Trabalhando como Engenheiro de Software.',
       linkedinDetails: 'veja minha carreira',
       githubDetails: 'acompanhe meus projetos',
@@ -34,7 +39,12 @@ export class Home implements OnInit {
     },
     en: {
       name: 'Gustavo Barez',
-      title: 'Software Engineer',
+      titles: [
+        'Software Engineer',
+        'Full Stack Developer',
+        'Frontend Developer',
+        'Backend Developer',
+      ],
       bio: 'Brazilian obsessed with technology for a decade. Working as a Software Engineer.',
       linkedinDetails: 'see my career',
       githubDetails: 'follow my projects',
@@ -42,9 +52,13 @@ export class Home implements OnInit {
       cvButton: 'Download my CV',
     },
   };
-
   currentLang: 'pt' | 'en' = 'en';
   content = this.textData.en;
+  currentTitle: string = '';
+  currentTitleIndex: number = 0;
+  currentCharIndex: number = 0;
+  isTyping: boolean = true;
+  typingInterval: any;
 
   constructor(public theme: Theme) {}
 
@@ -54,6 +68,39 @@ export class Home implements OnInit {
       this.currentLang = savedLang;
       this.content = this.textData[this.currentLang];
     }
+    this.startTypingAnimation();
+  }
+
+  ngOnDestroy(): void {
+    if (this.typingInterval) {
+      clearInterval(this.typingInterval);
+    }
+  }
+
+  startTypingAnimation(): void {
+    this.typingInterval = setInterval(() => {
+      if (this.isTyping) {
+        if (
+          this.currentCharIndex <
+          this.content.titles[this.currentTitleIndex].length
+        ) {
+          this.currentTitle +=
+            this.content.titles[this.currentTitleIndex][this.currentCharIndex];
+          this.currentCharIndex++;
+        } else {
+          this.isTyping = false;
+          setTimeout(() => {
+            if (!this.isTyping) {
+              this.isTyping = true;
+              this.currentTitle = '';
+              this.currentCharIndex = 0;
+              this.currentTitleIndex =
+                (this.currentTitleIndex + 1) % this.content.titles.length;
+            }
+          }, 2000);
+        }
+      }
+    }, 100);
   }
 
   toggleTheme(): void {
@@ -64,5 +111,8 @@ export class Home implements OnInit {
     this.currentLang = this.currentLang === 'en' ? 'pt' : 'en';
     this.content = this.textData[this.currentLang];
     localStorage.setItem('language', this.currentLang);
+    this.currentTitle = '';
+    this.currentCharIndex = 0;
+    this.isTyping = true;
   }
 }
